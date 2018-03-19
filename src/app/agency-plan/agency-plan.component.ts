20248cd954fbb00ac061640a8ba4e95742c00bd5
cyrilmp <cytilmp@gmail.com>
2018-03-19 19:16:17 +0100

    style map

--- src/app/agency-plan/agency-plan.component.ts
+++ src/app/agency-plan/agency-plan.component.ts
@@ -25,20 +25,21 @@ export class AgencyPlanComponent implements OnInit {
     stage1.markers.push(new Marker(1111,"Pole RH","50","90",1));
     stage1.markers.push(new Marker(2221,"Projet 2","50","10",2));
     stage1.markers.push(new Marker(3331,"Bureau d'Eric","20","50",3));
+    stage1.picture = "assets/images/map.JPG";
     this.agency.stages.push(stage1);
 
     let stage2: Stage = new Stage(2,"Etage 1");
-    stage2.markers.push(new Marker(4441,"WC","50","90"));
-    stage2.markers.push(new Marker(1112,"Pole Support","50","10",4));
-    stage2.markers.push(new Marker(3332,"Bureau Jeanne","20","50",5));
-    stage2.picture = "https://thumbs.dreamstime.com/z/plan-d-%C3%A9tage-14650802.jpg";
+    stage2.markers.push(new Marker(4441,"WC","10","50"));
+    stage2.markers.push(new Marker(1112,"Pole Support","50","20",4));
+    stage2.markers.push(new Marker(3332,"Bureau Jeanne","80","30",5));
+    stage2.picture = "assets/images/map.JPG";
     this.agency.stages.push(stage2);
 
     let stage3: Stage = new Stage(3,"Etage 2");
     stage3.markers.push(new Marker(3333,"Bureau Michelle","50","90",6));
     stage3.markers.push(new Marker(2222,"Projet PSA","50","10",7));
     stage3.markers.push(new Marker(1113,"Pole SAV","80","50",8));
-    stage3.picture = "https://breizhhouse2013.files.wordpress.com/2012/10/plan-etage.png";
+    stage3.picture = "assets/images/map.JPG";
     this.agency.stages.push(stage3);
 
 
